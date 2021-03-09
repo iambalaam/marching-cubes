4021ca27f0d26333236b5011dad131ddac540c7d
Guy Balaam <guy.balaam@gmail.com>
2021-03-09 10:50:32 +0000

    My first unity unit tests

---
+++ Marching Cubes/Assets/Tests/Static/TriangulationTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    public class TriangulationTests
+    {
+        [Test]
+        public void TriangulationsAreMultiplesOf3()
+        {
+            foreach(int[] tris in Triangulation.lookupTable)
+            {
+                Assert.True(tris.Length % 3 == 0, "Lookup entry wasn't a multiple of 3");
+            }
+        }
+
+        [Test]
+        public void AllTriangulations()
+        {
+            double allPossibleTriangulations = Math.Pow(2, 8);
+            Assert.AreEqual(allPossibleTriangulations, Triangulation.lookupTable.Length);
+        }
+
+    }
+}
