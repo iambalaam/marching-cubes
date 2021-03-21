cb5c032ab0d23a3189989a721f2e0c3addaca9b4
Guy Balaam <guy.balaam@gmail.com>
2021-03-21 13:43:27 +0000

    Adding test scene to debug terrain generation

---
+++ Marching Cubes/Assets/Scripts/NoiseTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseTest : MonoBehaviour
+{
+    // Exposed in unity
+    public Material material;
+
+    // Members
+    private Vector3Int _chunkSize = new Vector3Int(20, 50, 20);
+
+    private void Start() => GenerateChunks();
+
+    private void GenerateChunks()
+    {
+        var chunk = new GameObject($"Chunk");
+        var meshGen = chunk.AddComponent<MeshGenerator>();
+        chunk.GetComponent<MeshRenderer>().material = material;
+        chunk.transform.parent = transform;
+        meshGen.Initialize(Vector3Int.zero, _chunkSize);
+    }
+}
